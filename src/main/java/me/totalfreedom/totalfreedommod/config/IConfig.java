d2884f007bde1a8ea0b118af37f8f4411c015cdc
Focusvity <nathan.curran10012@gmail.com>
2022-10-29 21:36:05 +1100

    Remove ParseException from IConfig

--- src/main/java/me/totalfreedom/totalfreedommod/config/IConfig.java
+++ src/main/java/me/totalfreedom/totalfreedommod/config/IConfig.java
@@ -1,11 +1,10 @@
 package me.totalfreedom.totalfreedommod.config;
 
-import java.text.ParseException;
 import org.bukkit.configuration.ConfigurationSection;
 
 public interface IConfig
 {
-    void loadFrom(ConfigurationSection cs) throws ParseException;
+    void loadFrom(ConfigurationSection cs);
 
     void saveTo(ConfigurationSection cs);
 
